c515276330f5f1cdca2f9aebba60e570404d457f
Your Name <you@example.com>
2019-10-19 18:43:08 +0900

    sumiweb

--- event.php
+++ event.php
@@ -2,7 +2,7 @@
   <h1>イベント</h1>
   <div class="">
     <ul>
-      <li>学会参加（インタラクション）</li>
+      <li>学会参加（<a href="https://www.interaction-ipsj.org/2019/" target="_blank">インタラクション</a>）</li>
       <li>勉強会</li>
       <li>花見</li>
       <li>モーションキャプチャー講習会</li>
@@ -12,13 +12,14 @@
       <li>ハッカソン：
         <a href="https://hl2019.we-are-ma.jp/" target="_blank">
           ヒーローズリーグ</a>
-          (<a href="https://protopedia.net/taxonomy/term/1859">函館ハッカソン2019</a>)
+          (<a href="https://protopedia.net/taxonomy/term/1859" target="_blank">函館ハッカソン2019</a>)
         </li>
       <li>オープンラボ</li>
       <li>中間報告書/中間発表</li>
       <li>インタラクション執筆</li>
       <li>最終報告書/最終発表</li>
-      <li>学会発表（インタラクション）</li>
+
+      <li>学会発表（<a href="https://www.interaction-ipsj.org/2020/" target="_blank">インタラクション</a>）</li>
       <li>卒業</li>
     </ul>
   </div>
