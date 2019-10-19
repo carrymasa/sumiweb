d454cc35200b6901a35ffdc78b74cdf29fc863e0
Your Name <you@example.com>
2019-10-19 18:46:24 +0900

    sumiweb

--- event.php
+++ event.php
@@ -1,26 +1,24 @@
 <body>
   <h1>イベント</h1>
-  <div class="">
-    <ul>
-      <li>学会参加（<a href="https://www.interaction-ipsj.org/2019/" target="_blank">インタラクション</a>）</li>
-      <li>勉強会</li>
-      <li>花見</li>
-      <li>モーションキャプチャー講習会</li>
-      <li>CHI勉強会</li>
-      <li>知能情報交換会</li>
-      <li>夏休み</li>
-      <li>ハッカソン：
-        <a href="https://hl2019.we-are-ma.jp/" target="_blank">
-          ヒーローズリーグ</a>
-          (<a href="https://protopedia.net/taxonomy/term/1859" target="_blank">函館ハッカソン2019</a>)
-        </li>
+  <p>今年のイベント一覧</p>
+  <ul>
+    <li>学会参加（<a href="https://www.interaction-ipsj.org/2019/" target="_blank">インタラクション2019</a>）</li>
+    <li>勉強会</li>
+    <li>花見 in 五稜郭公園</li>
+    <li>モーションキャプチャー講習会</li>
+    <li><a href="https://sigchi.jp/seminar/chi2019/" target="_blank">CHI勉強会</a></li>
+    <li>知能情報交換会</li>
+    <li>夏休み</li>
+    <li>ハッカソン：
+      <a href="https://hl2019.we-are-ma.jp/" target="_blank">
+        ヒーローズリーグ</a>
+        (<a href="https://protopedia.net/taxonomy/term/1859" target="_blank">函館ハッカソン2019</a>)
+      </li>
       <li>オープンラボ</li>
       <li>中間報告書/中間発表</li>
       <li>インタラクション執筆</li>
       <li>最終報告書/最終発表</li>
-
-      <li>学会発表（<a href="https://www.interaction-ipsj.org/2020/" target="_blank">インタラクション</a>）</li>
+      <li>学会発表（<a href="https://www.interaction-ipsj.org/2020/" target="_blank">インタラクション2020</a>）</li>
       <li>卒業</li>
     </ul>
-  </div>
-</body>
+  </body>
