85f578c01bf23d670441ae2801f48c15f697965c
Your Name <you@example.com>
2019-10-19 13:12:58 +0900

    sumiweb

--- index.php
+++ index.php
@@ -39,6 +39,9 @@
     </div>
   </div>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="js/jquery.min.js">
+
+  </script>
   <script src="js/index.js"></script>
   <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
